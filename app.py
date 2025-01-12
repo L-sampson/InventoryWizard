bca7a0a7604528b5f55efb550d8ad6813f7560b7
unknown <lavontech@gmail.com>
2025-01-12 15:48:24 -0500

    Added remove button to remvoe pending files

--- app.py
+++ app.py
@@ -45,7 +45,7 @@ def drop_files(event):
         )
 
 # Import files dialog
-def select_files() ->int:
+def select_files() -> int:
     filetypes = (
         ('Excel Files', '*.xlsx;*xlsm;*.xls'),
         ('CSV Files', '*.csv')
@@ -70,7 +70,16 @@ def save_files(files: List[str]):
         msg.showinfo("Conversion Complete", f"Files have been saved to {save_directory}")
     else:
         msg.showerror("No Directory Selected", "Please select a directory to save the files.")
-    
+
+def remove_files():
+    file_count = file_listbox.size()
+    if file_count > 0:
+        confirm = msg.askyesno("Remove files", f"Would you like to remove {file_count} files from conversion?")
+        if confirm:
+            msg.showinfo("Removed Files", f"Removed {file_count} file!")
+            file_listbox.delete(0, END)
+    else:
+        msg.showerror("Empty Files List", "No files are pending conversion")    
     
 def convert_files():
     file_count = file_listbox.size()
@@ -106,7 +115,7 @@ drop_image = Image.open("assets/file-upload.png")
 resized = drop_image.resize((50, 50))
 upload_logo = ImageTk.PhotoImage(resized)
 
-drop_zone =  Frame(tool_bar, bg="white", highlightbackground="#0077C8", highlightthickness=2, height=200, width=400)
+drop_zone = Frame(tool_bar, bg="white", highlightbackground="#0077C8", highlightthickness=2, height=200, width=400)
 drop_zone.pack(pady=20)
 drop_zone.drop_target_register(DND_FILES)
 drop_zone.dnd_bind('<<Drop>>', drop_files)
@@ -122,8 +131,11 @@ file_entry_frame = Frame(tool_bar, bg="white")
 file_entry_frame.pack(fill=X, padx=20, pady=10)
 
 file_entry_label = Label(file_entry_frame,text="Select Files to be Converted: ", font=('Arial', 12, 'bold'), bg="white")
-file_entry_label.pack(anchor=W)
+file_entry_label.grid(row=0, column=0, padx=5, pady=5, sticky=W)
 
+button_font = tkFont.Font(family="Helvetica", size=12, weight="bold")
+import_file_btn = Button(file_entry_frame, text="Import File(s)", width=15, height=1, command=select_files, bg="#0077C8", fg="white", font=button_font)
+import_file_btn.grid(row=0, column=1, padx=5, pady=5, sticky=W)
 file_listbox = Listbox(file_entry_frame, width=60, height=5, font=("Arial", 10, "italic"), bd=2, relief="groove")
 
 # Button Section
@@ -131,8 +143,8 @@ button_frame = Frame(tool_bar, bg="white")
 button_frame.pack(pady=1)
 
 button_font = tkFont.Font(family="Helvetica", size=12, weight="bold")
-import_file_btn = Button(button_frame, text="Import File(s)", width=15, height=1, command=select_files, bg="#0077C8", fg="white", font=button_font)
-import_file_btn.grid(row=0, column=0, padx=10)
+remove_btn = Button(button_frame, text="Remove Files(s)", width=15, height=1, bg="#FE0944", font=button_font, fg="white", command=remove_files)
+remove_btn.grid(row=0, column=0, padx=10)
 
 convert_file_btn = Button(button_frame, text="Convert File(s)", width=15, height=1, command=convert_files, bg="#00B28F", fg="white", font=button_font)
 convert_file_btn.grid(row=0, column=1, padx=10)
@@ -140,4 +152,4 @@ convert_file_btn.grid(row=0, column=1, padx=10)
 file_entry_label.grid(row=0, column=0, padx=5, pady=5, sticky=W)
 file_listbox.grid(row=1, column=0, columnspan=3, padx=5, pady=5, sticky=W+E)
 
-root.mainloop() 
+root.mainloop()
